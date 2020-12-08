c7afe90841f4041d1073b2c2bccd2cbd55015730
Gulnara M <gulnara@modeanalytics.com>
2020-12-07 21:03:39 -0500

    day 3 setup

---
+++ day3_pt1.rb
@@ -0,0 +1,19 @@
+
+def count_trees(map)
+	count = 0
+	right = 3
+	down = 1
+
+	empty = "."
+	tree = "#"
+
+	for i in 0..(map.length - 1)
+
+
+	end
+end
+
+
+map = File.read("trees.txt").split
+
+puts count_trees(map)
