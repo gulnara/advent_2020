17638def89b93d88548cd68a3a36e356e45050d8
Gulnara M <gulnara@modeanalytics.com>
2020-12-09 10:20:48 -0500

    adding case statements

--- day4_pt2.rb
+++ day4_pt2.rb
@@ -16,14 +16,14 @@ def validate_passport(data)
 	split_data = Set.new(data.split(" ").map {|e| e.split(":")})
 	valid_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 	split_data.each do |field|
-		puts "#{field[0]}"
-		if !valid_field(field)
+		if !validate_field(field)
 			return false
 		else
 			valid_fields.delete(field[0])
 		end
 	end
 
+	puts "#{valid_fields}"
 	if valid_fields.length == 0
 		return true
 	else
@@ -31,16 +31,65 @@ def validate_passport(data)
 	end
 end
 
-	# valid_fields = {
-	# 	"byr": [1920, 2002], 
-	# 	"iyr": [2010, 2020], 
-	# 	"eyr": [2020, 2030], 
-	# 	"hgt": [
-	# 		{"cm": [150, 193]}, 
-	# 		{"in": [59, 76]}], 
-	# 	"hcl": [6, [0,9], ["a","f"]], 
-	# 	"ecl": ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"], 
-	# 	"pid": 9]
+def validate_field(field)
+	puts "#{field}"
+	case field[0]
+
+	when "byr"
+		puts "#{field[1]}"
+		if field[1].to_i >= 1920 or field[1].to_i <= 2002
+			return true
+		else
+			return false
+		end
+	when "iyr"
+		puts "#{field[1]}"
+		if field[1].to_i >= 2010 or field[1].to_i <= 2020
+			return true
+		else
+			return false
+		end
+	when "eyr"
+		puts "#{field[1]}"
+		if field[1].to_i >= 2020 or field[1].to_i <= 2030
+			return true
+		else
+			return false
+		end
+	when "hgt"
+		puts "#{field[1]}"
+		if field[1].to_i >= 1920 or field[1].to_i <= 2002
+			return true
+		else
+			return false
+		end
+	when "hcl"
+		puts "#{field[1]}"
+		if field[1].to_i >= 1920 or field[1].to_i <= 2002
+			return true
+		else
+			return false
+		end
+	when "ecl"
+		puts "#{field[1]}"
+		if field[1].to_i >= 1920 or field[1].to_i <= 2002
+			return true
+		else
+			return false
+		end
+	when "pid"
+		puts "#{field[1]}"
+		if field[1].to_i >= 1920 or field[1].to_i <= 2002
+			return true
+		else
+			return false
+		end
+	else
+		puts "#{field}"
+		return true
+	end
+end
+
 
 
 def count_valid_passports(parsed_data)
