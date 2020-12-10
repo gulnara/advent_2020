36c9ff522419c3dc38abfbd36cf5cb05b6abe43d
Gulnara M <gulnara@modeanalytics.com>
2020-12-09 22:08:05 -0500

    added pt 2 of day 5

--- day5_pt1.rb
+++ day5_pt1.rb
@@ -47,7 +47,7 @@ def seat_id(column, row)
 	return (row * 8 + column)
 end
 
-def highest_seat_id(seats)
+def create_seats_array(seats)
 	seat_ids = []
 	seats.each do |seat|
 		row = which_row(seat.slice(0..6))
@@ -55,10 +55,33 @@ def highest_seat_id(seats)
 		seat_ids.append(seat_id(column, row))
 	end
 
-	return seat_ids.sort!.last
+	return seat_ids.sort!
+end
+
+def highest_seat_id(seats_data)
+	seats = create_seats_array(seats_data)
+	return seats.last
+end
+
+def missing_seat_id(seats_data)
+	seats = create_seats_array(seats_data)
+	idx1 = 0
+	idx2 = idx1 + 1
+
+	for i in 0..(seats.length - 2)
+		j = i + 1
+		if seats[j] != seats[i] + 1 
+			return (seats[i] + 1)
+		end
+	end
+	return "no missing seat"
 end
 
 seats = File.read("seats.txt").split
 
 puts highest_seat_id(seats)
 # 901
+
+puts missing_seat_id(seats)
+# 661
+
