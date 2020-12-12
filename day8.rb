4639866072637e15126b66044733525c423a194c
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-12-11 19:56:23 -0500

    day 8 pt1

--- day8.rb
+++ day8.rb
@@ -1,9 +1,44 @@
 
 
+def acc_value(ops)
+
+	acc = 0
+	i = 0
+
+	visited = []
+
+	while !visited.include? i
+		o = ops[i][0]
+		# inc = ops[i][1].slice(1..).to_i
+		inc = ops[i][1].to_i
+		visited.append(i)
+		if o == "nop"
+			i += 1
+		elsif o == "acc"
+			acc += inc
+			i += 1
+		elsif o == "jmp"
+			i += inc
+		else
+			puts "something wrong"
+		end
+	end
+
+	return acc
+
+end
+
+
 ops = []
 
-File.foreach("temp.txt").each do |line|
+File.foreach("operations.txt").each do |line|
 	ops.append(line.split(" "))
 end
 
-puts ops
+# puts ops
+
+puts acc_value(ops)
+# 1749
+
+
+
