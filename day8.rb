1cb28d9d7a7b3c6f2e363320674a651baf568136
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-12-12 09:34:57 -0500

    solution for day 8 pt2

--- day8.rb
+++ day8.rb
@@ -1,25 +1,30 @@
 
 
 def acc_value(ops)
-
+	puts "got here"
 	acc = 0
 	i = 0
 
 	visited = []
 
 	while !visited.include? i
-		o = ops[i][0]
-		inc = ops[i][1].to_i
-		visited.append(i)
-		if o == "nop"
-			i += 1
-		elsif o == "acc"
-			acc += inc
-			i += 1
-		elsif o == "jmp"
-			i += inc
+		if i != ops.length
+			o = ops[i][0]
+			inc = ops[i][1].to_i
+			visited.append(i)
+			if o == "nop"
+				i += 1
+			elsif o == "acc"
+				acc += inc
+				i += 1
+			elsif o == "jmp"
+				i += inc
+			else
+				puts "something wrong"
+			end
 		else
-			puts "something wrong"
+			puts "reached end"
+			return acc, i
 		end
 	end
 
@@ -29,22 +34,54 @@ end
 
 def replace_jmp(ops)
 
+	map = {}
+
 	for o in 0..(ops.length - 1)
+		# puts ops[o]
 		if ops[o][0] == "jmp"
 			new_ops = ops
 			new_ops[o][0] = "nop"
+			puts "#{new_ops}"
 			acc, i = acc_value(new_ops)
-			if i == ops.length + 1
-				return acc, i
-			end
+			map[i] = acc
+			new_ops[o][0] = "jmp"
+			puts map
 		end
+	end
+	return map
+end
+
+def replace_nop(ops)
+
+	map = {}
+
+	for o in 0..(ops.length - 1)
+		# puts ops[o]
+		if ops[o][0] == "nop"
+			new_ops = ops
+			new_ops[o][0] = "jmp"
+			puts "#{new_ops}"
+			acc, i = acc_value(new_ops)
+			map[i] = acc
+			new_ops[o][0] = "nop"
+			puts map
+		end
+	end
+	return map
+end
+
+def find_right_rep(map1, map2, l)
+	if map1.key?(l)
+		return map1[l]
+	else
+		return map2[l]
 	end
 end
 
 
 ops = []
 
-File.foreach("temp.txt").each do |line|
+File.foreach("operations.txt").each do |line|
 	ops.append(line.split(" "))
 end
 
@@ -53,8 +90,11 @@ end
 # puts acc_value(ops)
 # 1749
 
-puts replace_jmp(ops)
+map1 = replace_jmp(ops)
+map2 =  replace_nop(ops)
 
+puts find_right_rep(map1, map2, ops.length)
+# 515
 
 
 
