0dcc966a2523cbc714c73d16bd734a090663f20b
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-12-13 17:59:09 -0500

    solution for day 9 pt 2

--- day9.rb
+++ day9.rb
@@ -41,6 +41,49 @@ def first_odd(nums, pream_length)
 
 end
 
+def calc_min_max()
+
+end
+
+def find_set(nums, odd_num)
+
+	set = []
+	temp_sum = 0
+
+	nums.each do |i|
+		puts "i #{i}"
+		temp_sum += i
+		puts "temp sum #{temp_sum}"
+		
+
+		if temp_sum < odd_num
+			set.append(i)
+			puts "set #{set}"
+		elsif temp_sum > odd_num
+			# puts i
+			while temp_sum > odd_num
+				temp_sum = temp_sum - set[0]
+				puts "this t #{temp_sum}"
+				set.shift
+			end
+			set.append(i)
+			if temp_sum == odd_num
+				min = set.min
+				max = set.max
+				return min + max
+			end
+			puts "set #{set}"
+		elsif temp_sum == odd_num
+			min = set.min
+			max = set.max
+			return min + max
+		end
+
+	end
+
+
+end
+
 
 nums = []
 pream_length = 25
@@ -49,7 +92,12 @@ File.foreach("day_9_input.txt").each do |line|
 	nums.append(line.to_i)
 end
 
-puts first_odd(nums, pream_length)
+# puts first_odd(nums, pream_length)
 # 41682220
+
+
+odd_num = 41682220
+
+puts find_set(nums, odd_num)
 
 
