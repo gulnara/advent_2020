18e9bf7345aafa2a6681a0ab3cb7955ec4c7d539
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-12-14 21:52:11 -0500

    getting neoughbours differently

--- day11.rb
+++ day11.rb
@@ -1,62 +1,56 @@
 
 
-def can_occupy?(r1, r2, r3, i)
-	mini_map = [r1[0][i - 1], r1[1][i], r1[2][i + 1], r2[0][i - 1], r2[2][i + 1], r3[0][i - 1], r3[1][i], r3[2][i + 1]]
 
-	if mini_map.include? "#"
-		return false
-	elsif r2[1][i] == "."
-		return false
-	else
-		return true
-	end
+def can_occupy?(stack, row, i)
+
 end
 
 def should_vacate?(r1, r2, r3, i)
-	mini_map = [r1[0][i - 1], r1[1][i], r1[2][i + 1], r2[0][i - 1], r2[2][i + 1], r3[0][i - 1], r3[1][i], r3[2][i + 1]]
-	ocupied_seats = 0
-	mini_map.each do |s|
-		if s == "#"
-			ocupied_seats += 1
-		end
-	end
 
-	if r2[1][i] == "#" and ocupied_seats >= 4
-		return true
-	else
-		return false
-	end
+end
+
+def get_neighbours(map, r, c, width, length)
+
+	neighbours = []
+
+	neighbours.push map[r - 1][c] unless r == 0 # N
+  neighbours.push map[r - 1][c + 1] unless r == 0 or c == width - 1 # NE
+  neighbours.push map[r][c + 1] unless c == width - 1 # E
+  neighbours.push map[r + 1][c + 1] unless r == length - 1 or c == width - 1 # SE
+  neighbours.push map[r + 1][c] unless r == length - 1 # S
+  neighbours.push map[r + 1][c - 1] unless r == length - 1 or c == 0 # SW
+  neighbours.push map[r][c - 1] unless c == 0 # W
+  neighbours.push map[r - 1][c - 1] unless r == 0 or c == 0  # NW
+
+  return neighbours
 end
 
 def scan_map(map)
-	# a = 1
-	# k = 1
-	seat_stack = []
-	width = map[0].length
 
+	width = map[0].length
+	length = map.length
 	new_map = []
 
+	length.times do
+    new_map.push [nil] * width
+  end
 	
-	for j in 0..(map.length - 1)
-		for i in 0..(width - 1)
-			if seat_stack.length < 3
-				seat_stack.append(map[j])
-				new_map.append(map[j])
-			else
-				if can_occupy?(seat_stack[0], seat_stack[1], seat_stack[2], i)
-					seat_stack[1][i] = "#"
-					new_map[j+1][i] = "#"
-				elsif should_vacate?(seat_stack[0], seat_stack[1], seat_stack[2], i)
-					seat_stack[1][i] = "L"
-					new_map[j+1][i] = "L"
-				end
-				seat_stack.shift
-			end
+	for r in 0..(length - 1)
+		for c in 0..(width - 1)
+			neighbours = get_neighbours(map, r, c, width, length)
+
+			if map[r][c] == "L" and neighbours.none? "#"
+        new_map[r][c] = "#"
+      elsif map[r][c] == "#" and neighbours.count("#") >= 4
+        new_map[r][c] = "L"
+      else
+        new_map[r][c] = map[r][c]
+      end
 		end
-		j += 1
 	end
 
-	return new_map
+	puts map.length
+	return new_map.length
 end
 
 seat_map = []
