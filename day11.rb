e68cb7fbbb3e84af3994d6f8bacf2b3668da4225
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-12-15 20:53:27 -0500

    refactored day 11

--- day11.rb
+++ day11.rb
@@ -1,10 +1,22 @@
 
+$map = []
 
+File.foreach("temp.txt") do |line|
+	line = line.split("")
+	if line[-1] == "\n"
+		line = line[0..-2]
+	end
+	$map.append(line)
+end
 
-def count_occupied(map)
+$width = $map[0].length
+$length = $map.length
+
+def count_occupied
 	count = 0
-	map.each do |i|
+	$map.each do |i|
 		i.each do |j|
+			puts j
 			if j == "#"
 				count += 1
 			end
@@ -14,65 +26,51 @@ def count_occupied(map)
 end
 
 
-def get_neighbours(map, r, c, width, length)
+def get_neighbours(c, r)
 
 	neighbours = []
 
-	neighbours.push map[r - 1][c] unless r == 0 # N
-  neighbours.push map[r - 1][c + 1] unless r == 0 or c == width - 1 # NE
-  neighbours.push map[r][c + 1] unless c == width - 1 # E
-  neighbours.push map[r + 1][c + 1] unless r == length - 1 or c == width - 1 # SE
-  neighbours.push map[r + 1][c] unless r == length - 1 # S
-  neighbours.push map[r + 1][c - 1] unless r == length - 1 or c == 0 # SW
-  neighbours.push map[r][c - 1] unless c == 0 # W
-  neighbours.push map[r - 1][c - 1] unless r == 0 or c == 0  # NW
+	neighbours.push $map[r - 1][c] unless r == 0 # N
+  neighbours.push $map[r - 1][c + 1] unless r == 0 or c == $width - 1 # NE
+  neighbours.push $map[r][c + 1] unless c == $width - 1 # E
+  neighbours.push $map[r + 1][c + 1] unless r == $length - 1 or c == $width - 1 # SE
+  neighbours.push $map[r + 1][c] unless r == $length - 1 # S
+  neighbours.push $map[r + 1][c - 1] unless r == $length - 1 or c == 0 # SW
+  neighbours.push $map[r][c - 1] unless c == 0 # W
+  neighbours.push $map[r - 1][c - 1] unless r == 0 or c == 0  # NW
 
   return neighbours
 end
 
-def scan_map(map)
 
-	width = map[0].length
-	length = map.length
+current_occupied = 0
+last_occupied = nil
+
+until current_occupied == last_occupied
+	last_occupied = current_occupied
 	new_map = []
 
-	length.times do
-    new_map.push [nil] * width
-  end
-
-  current_occupied = 0
-	last_occupied = nil
-
-	until current_occupied == last_occupied
-  	last_occupied = current_occupied
-	
-		for r in 0..(length - 1)
-			for c in 0..(width - 1)
-				neighbours = get_neighbours(map, r, c, width, length)
-
-				if map[r][c] == "L" and neighbours.none? "#"
-	        new_map[r][c] = "#"
-	      elsif map[r][c] == "#" and neighbours.count("#") >= 4
-	        new_map[r][c] = "L"
-	      else
-	        new_map[r][c] = map[r][c]
-	      end
-			end
-		end
-
-		map = new_map
-		current_occupied = count_occupied(map)
+	$length.times do
+  	new_map.push [nil] * $width
 	end
 
-	return current_occupied
+	(0...$length).each do |r|
+  	(0...$width).each do |c|
+			neighbours = get_neighbours(c, r)
+
+			if $map[r][c] == "L" and neighbours.none? "#"
+        new_map[r][c] = "#"
+      elsif $map[r][c] == "#" and neighbours.count("#") >= 4
+        new_map[r][c] = "L"
+      else
+        new_map[r][c] = $map[r][c]
+      end
+		end
+	end
+
+	$map = new_map
+	current_occupied = count_occupied
+	puts current_occupied
 end
 
-seat_map = []
-
-File.foreach("temp.txt") do |line|
-	line = line.split("")
-	seat_map.append(line)
-end
-
-puts "#{scan_map(seat_map)}"
 
